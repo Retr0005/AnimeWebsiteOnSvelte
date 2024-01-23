e2197e9ee5705c957181b4c0737332da451b6c45
Retr0005 <gianlu.storny@gmail.com>
2024-01-23 20:27:14 +0100

    still ass

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
+
 <script>
     import { onMount } from 'svelte';
   
@@ -71,6 +72,9 @@
   
   
   onMount(() => {
+    const interval = setInterval(nextImage, 5000); // Cambia immagine ogni 5000 millisecondi (5 secondi)
+
+  
     for (let i = 0; i < 1; i++) {
       setTimeout(async () => {
         try {
@@ -99,33 +103,71 @@
         }
       }, i * delay);
     }
+
+    return () => clearInterval(interval); // Pulisce l'intervallo quando il componente viene distrutto
+
   });
+
+
+
+
+
+  let currentImageIndex = 0;
+  const images = [
+    'https://www.animeunity.to/images/Sfondo2.png',
+
+    /*
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fbanner-anime%2F33&psig=AOvVaw0XJ8jBe-ISVGvgjg2bJOi6&ust=1706084945790000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCPDzv_mL84MDFQAAAAAdAAAAABAD',
+    'https://i0.wp.com/wallpapercave.com/wp/wp3534292.jpg',
+*/
+
+  ];
+
+  function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+  }
+
+  function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  }
+
   
   </script>
 
-<div>
-	<img class="h-auto max-w-full" src="https://www.animeunity.to/images/Sfondo2.png" alt="">
+
+
+<div class="immagine_iniziale">
+  <button style=" position: absolute; top: 50%; left: 10px" class="btn btn-sm bg-indigo-600 bg-opacity-75 text-white text-opacity-75" on:click={prevImage} >&lt;</button>
+  <img style="max-width: 100%; height: auto;" src={images[currentImageIndex]} alt="">
+  <button style=" position: absolute; top: 50%; right: 10px" class="btn btn-sm bg-indigo-600 bg-opacity-75 text-white text-opacity-75" on:click={nextImage}>&gt;</button>
 </div>
-<img class="h-auto max-w-full rounded-lg " url="https://www.animeunity.to/images/Sfondo2.png" alt="">
+
+
+
 <div class="container flex">
+
 <blockquote class="my-4 border-s-8 border-indigo-600 justify-center ml-44 my-8 ">
     <p class="font-sans text-7xl font-medium ">Trending anime</p>
 </blockquote>
-<p class="font-sans text-4xl font-medium my-16 ml-40 ">Search:</p>
-<input class="input max-h-12 ml-4 max-w-56 my-16" type="search" name="demo" placeholder="Soy_Sofia" />
+<p class="font-sans text-4xl font-medium my-16 ml-40 ">search:</p>
+<input class="input max-h-12 ml-4 max-w-56 my-16" type="search" name="demo" placeholder="Search..." />
 </div>
 
 
+
+
+
 <div class="container flex flex-wrap justify-center items-center mx-auto gap-x-4 gap-y-4 mb-8 ">
-  {#each animeList as { id, imageUrl, title}}
-    <div class="box">
-      <div class="card h-[432px] w-64">
-        <header class="card-header  "><img class="max-h-[320px]" src="{imageUrl}"  on:click={() => handleAnimeClick(id)} alt=""></header>
-        <section class="p-4 text-center font-bold max-h-[56px] ">{title}</section>
-        <footer class="card-footer text-center font-semibold">streaming now!</footer>
-      </div>
-    </div>
-  {/each}
+	{#each animeList as { id, imageUrl, title}}
+  <div class="box">
+	<div class="card h-[432px] w-64">
+		<header class="card-header  "><img class="max-h-[320px]" src="{imageUrl}"  on:click={() => handleAnimeClick(id)} alt=""></header>
+		<section class="p-4 text-center font-bold max-h-[56px] ">{title}</section>
+		<footer class="card-footer text-center font-semibold">streaming now!</footer>
+	</div>
+</div>
+
+	{/each}
 </div>
 <div  class="">
 	<ol class="breadcrumb flex justify-center mb-4">
@@ -138,10 +180,4 @@
 
   </ol>
 </div>
-
-<style lang="css">
-  @import '../app.css';
-</style>
-
-
 
--- src/routes/details/+page.svelte
+++ src/routes/details/+page.svelte
@@ -30,28 +30,48 @@
       console.error('Errore durante la richiesta dei dati dell\'anime:', error);
     }
   }
-  
+
 </script>
 
 {#if animeData}
-  <div class="display">
-    <h1 class="h1 font-sans text-7xl font-medium justify-center items-center mx-auto text-center my-8 ">{animeData.data.title_english}</h1>
-    <embed class="mx-auto h-full w-4/5 my-8" type="video/webm" src="{animeData.data.trailer.embed_url}">
-      <p> {animeData.data.synopsis}</p>
+  <div class="container items-center display mx-auto">
+
+    <div class="title card wrapper">
+      <h1 class="font-bold text-6xl">{animeData.data.title}</h1>
+    </div>
+      <br>
+    <div class="wrapper card">
+      <div class="image card">
+        <img src="{animeData.data.images.jpg.image_url}"> 
+      </div>
+
+      <div class="details mx-auto">
+        <h1>Titolo originale: {animeData.data.title_japanese}</h1>
+        <p>Durata: {animeData.data.duration}</p>
+        <p>Episodi: {animeData.data.episodes}</p>
+
+        <p>Popolarità: {animeData.data.popularity}</p>
+        <p>Rank: {animeData.data.rank}</p>
+
+        <p>Anno: {animeData.data.year}</p>
+        <p>Streaming: {animeData.data.streaming[0].url}</p>
+      </div>
+    </div>
+
+      <br>
+    <div class="wrapper card">
+      <div class="container trailer mx-auto card">
+        <iframe src="{animeData.data.trailer.embed_url}" frameborder="0" width="100%" height="1100%"></iframe>
+      </div>
+      <div class="syn mx-auto" width="50">
+        <p> {animeData.data.synopsis}</p>
+      </div>
+    </div>
   </div>
-  
+
 
   
   <!----<div id="anime-details">
-    <h1>{animeData.data.title_japanese}</h1>
-    <p>Durata: {animeData.data.duration}</p>
-    <p>Episodi: {animeData.data.episodes}</p>
-  
-    <p>Popolarità: {animeData.data.popularity}</p>
-    <p>Rank: {animeData.data.rank}</p>
-    
-    <p>Anno: {animeData.data.year}</p>
-    <p>Streaming: {animeData.data.streaming[0].url}</p>
   </div>
 -->
 {/if}
